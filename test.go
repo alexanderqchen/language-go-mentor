c9ff33a82c79ea68c9a9f5dbcc76ddf8adaf6273
alexanderqchen <aqchen@ucla.edu>
2019-02-24 02:58:27 -0700

    initial commit

---
+++ test.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	////
+		for i in hello:
+			print(i)
+
+		if asdf:
+			asdf
+		else:
+			ok i guess
+
+		def test:
+			print('hello world')
+
+	////
+	fmt.Println("Hello, world.")
+}
